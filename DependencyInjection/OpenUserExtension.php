69a8299810fd57232808abde95f433bae45d1c51
Justin John Mathews <justin.johnzz@gmail.com>
2013-01-11 23:56:00 +0530

    Update DependencyInjection/OpenUserExtension.php

--- DependencyInjection/OpenUserExtension.php
+++ DependencyInjection/OpenUserExtension.php
@@ -24,5 +24,7 @@ class OpenUserExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    	$container->setParameter('open_oauth.firewall_name', $config);
+        
     }
 }

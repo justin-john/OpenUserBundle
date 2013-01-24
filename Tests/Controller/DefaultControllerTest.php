a3a572d3ea4ceffca3a1cbfb4f5df15f3771ba16
Justin John Mathews <justin.johnzz@gmail.com>
2013-01-24 18:26:34 +0530

    Update Tests/Controller/DefaultControllerTest.php

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,8 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/hello/Open');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Welcome to OpenUserBundle")')->count() > 0);
     }
 }
